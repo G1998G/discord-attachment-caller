9693beb69d4aa958f5b64b04aa5fc298b936a03c
G1998G <1998itogo@gmail.com>
2021-07-22 20:39:10 +0900

    部分一致検索機能&登録者検索機能を追加

--- image-db-bot.py
+++ image-db-bot.py
@@ -4,6 +4,7 @@ import sqlite3
 from contextlib import closing
 from disputils import BotEmbedPaginator
 from disputils.pagination import ControlEmojis
+from typing import Union
 
 # guild内投稿回数カウント用
 class C:
@@ -92,6 +93,51 @@ def registered_list(serverid):
         connection.close()
     return res  
 
+def partial_match(serverid,keyword):
+    dbname = 'database.db'
+    res = list()
+    keyword = f'%{keyword}%'
+    with closing(sqlite3.connect(dbname)) as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        sql = 'SELECT keyword,userid FROM discord_table WHERE serverid= ? AND keyword LIKE ?'
+        data = (serverid,keyword)
+        cursor.execute(sql, data)
+        for row in cursor:
+            res.append(f'**`{row["keyword"]}`** _`{bot.get_user(row["userid"]).name}`_\n')
+        connection.close()
+    return res  
+
+def search_author(serverid,userid):
+    dbname = 'database.db'
+    res = list()
+    with closing(sqlite3.connect(dbname)) as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        sql = 'SELECT keyword,userid FROM discord_table WHERE serverid= ? AND userid= ?'
+        data = (serverid,userid)
+        cursor.execute(sql, data)
+        for row in cursor:
+            res.append(f'**`{row["keyword"]}`**\n')
+        connection.close()
+    return res  
+
+class Msg:
+
+    @staticmethod
+    async def len_over(ctx):
+        await ctx.send(f'```キーワードはスペース含め20文字以内で入力してください。```')
+    
+    @staticmethod
+    async def no_key(ctx):
+        await ctx.send(f'```{ctx.prefix}{ctx.invoked_with}入力時は必ずキーワードを指定してください。```')
+    
+    @staticmethod
+    async def no_img(ctx,keyword):
+        await ctx.send(content=f'`キーワード:{keyword}で画像は登録されてません。`')
+
+
+
 class HelpCommand(commands.HelpCommand):
     def __init__(self):
         super().__init__()
@@ -99,6 +145,9 @@ class HelpCommand(commands.HelpCommand):
         self.command_attrs["description"] = "コマンドリストを表示します。"
 
     async def send_bot_help(self,mapping):
+        '''
+        ヘルプを表示するコマンド
+        '''
         content = ""
         for cog in mapping:
             # 各コグのコマンド一覧を content に追加していく
@@ -114,7 +163,7 @@ class HelpCommand(commands.HelpCommand):
             for command in command_list:
                 content += f"{self.context.prefix}{command.name}  `{command.help}`\n"
             content += "\n"
-        embed = discord.Embed(title="**コマンドリスト**",description=content,color=discord.Colour.dark_orange())
+        embed = discord.Embed(title="**コマンドリスト**",description=content)
         await self.get_destination().send(embed=embed)
         postc()
 
@@ -133,24 +182,30 @@ class BasicCommand(commands.Cog):
 
         '''
         _id = ctx.guild.id
-        if arg:
+        
+        if not arg:
+            # キーワードが入力されていない場合はその旨を伝える
+            await Msg.no_key(ctx)
+        else:
             arg = ' '.join(arg)
             res = search_keyword(serverid = _id,keyword = arg)
             if ctx.message.attachments:
-                # キーワードが10文字以上の場合は拒否
-                if len(arg) > 15:
-                    await ctx.send(content='`キーワードはスペース含め15文字以内に収めてください`')
-                # キーワードで登録がある場合は上書きするか尋ねる
+                if len(arg) > 20:
+                     # キーワードが10文字以上の場合は拒否
+                    await Msg.len_over(ctx)
+                
                 elif res:
-                    await ctx.send(content=f'`キーワード:{arg}は既にこの画像が登録されています。画像を上書きする場合は !okと入力してください。しない場合は!noと入力してください。`\n{res["content"]}')
+                    # キーワードで登録がある場合は上書きするか尋ねる
+                    await ctx.send(content=f'`キーワード:{arg}は既にこの画像が登録されています。画像を上書きする場合は {ctx.prefix}okと入力してください。しない場合は{ctx.prefix}noと入力してください。`\n{res["content"]}')
                     # 登録は１枚まで。
                     for attachment in ctx.message.attachments:
                         attachment = str(attachment)
                         self.update_file[str(_id)] = [arg,attachment]
                         print(self.update_file[str(_id)])
                         break
-                # キーワードに登録がない場合は画像登録
+                
                 else:
+                    # キーワードに登録がない場合は画像登録
                     # 登録は１枚まで。
                     for attachment in ctx.message.attachments:
                         attachment = str(attachment)
@@ -158,15 +213,14 @@ class BasicCommand(commands.Cog):
                         await ctx.send(content=f'`キーワード:{arg}で画像を登録しました。`')
                         break
 
-            # アタッチメントが無く、登録もない場合はその旨を伝える
+            
             elif not res:
-                await ctx.send(content=f'`キーワード:{arg}で画像は登録されてません。`')
-            # アタッチメントが無く、登録がある場合は登録画像を表示
+                # アタッチメントが無く、登録もない場合はその旨を伝える
+                await Msg.no_img(ctx,arg)
+            
             else:
+                # アタッチメントが無く、登録がある場合は登録画像を表示
                 await ctx.send(content=f'{res["content"]}')
-        # キーワードが入力されていない場合はその旨を伝える
-        else:
-            await ctx.send(content='`!imgを入力するときは必ずキーワードを指定してください。`')
         postc()
 
     @commands.command()
@@ -210,14 +264,19 @@ class DeleteCommand(commands.Cog):
         _id = ctx.guild.id
         if arg:
             arg = ' '.join(arg)
+            if len(arg) > 20:
+                await Msg.len_over(ctx)
+                postc()
+                return
+
             res = search_keyword(serverid = _id,keyword = arg)
             if res:
-                await ctx.send(content=f'`キーワード:{arg}に登録されたこの画像を削除しますか？　削除する場合は !delok, しない場合は !delnoと入力してください。`\n{res["content"]}')
+                await ctx.send(content=f'`キーワード:{arg}に登録されたこの画像を削除しますか？　削除する場合は {ctx.prefix}delok, しない場合は {ctx.prefix}delnoと入力してください。`\n{res["content"]}')
                 self.deletekeyword[str(_id)] = arg
             else:
-                await ctx.send(content=f'`キーワード:{arg}の登録はありません。`')
+                await Msg.no_img(ctx,arg)
         else:
-            await ctx.send(content='`削除する画像のキーワードを指定してください。`')
+            await Msg.no_key(ctx)
         postc()
 
     @commands.command()
@@ -230,9 +289,9 @@ class DeleteCommand(commands.Cog):
             keyword = self.deletekeyword[str(_id)]
             delete_dt(serverid=_id, keyword=keyword)
             await ctx.send(content=f'`キーワード:{keyword}に登録された画像を削除しました。`')
-            del self.deletekeyword[str(_id)],keyword
+            del self.deletekeyword[str(_id)]
         else:
-            await ctx.send(content=f'`このコマンドは登録画像削除実行用コマンドです。まずはdelok キーワードで削除する画像を指定してください`')
+            await ctx.send(content=f'`このコマンドは登録画像削除実行用コマンドです。まずは{ctx.prefix}delキーワードで削除する画像を指定してください`')
         postc()
 
     @commands.command()
@@ -244,9 +303,9 @@ class DeleteCommand(commands.Cog):
         if str(_id) in self.deletekeyword:
             keyword = self.deletekeyword[str(_id)]
             await ctx.send(content=f'`キーワード:{keyword}に登録された画像を削除しません。`')
-            del self.deletekeyword[str(_id)],keyword
+            del self.deletekeyword[str(_id)]
         else:
-            await ctx.send(content=f'`このコマンドは登録画像削除実行用コマンドです。まずはdelok キーワードで削除する画像を指定してください`')
+            await ctx.send(content=f'`このコマンドは登録画像削除実行用コマンドです。まずは{ctx.prefix}del キーワードで削除する画像を指定してください`')
         postc()
 
 
@@ -267,7 +326,7 @@ class ReferenceCommand(commands.Cog):
         if res:
             print(res,len(res))
             page = 1
-            embeds.append( discord.Embed(title=f"登録一覧(登録数:{len(res)}) {page}ページ目",description=f"{''.join(res[0:19])}",color=discord.Colour.dark_orange()) )
+            embeds.append( discord.Embed(title=f"登録一覧(登録数:{len(res)}) {page}ページ目",description=f"{''.join(res[0:19])}") )
             if len(res) > 20:
                 q = len(res) // 20
                 mod = len(res) % 20
@@ -276,13 +335,13 @@ class ReferenceCommand(commands.Cog):
                 while x < q:
                     page += 1
                     content1 = res[_index:_index+20]
-                    embeds.append( discord.Embed(title=f"登録一覧(登録数:{len(res)}) {page}ページ目",description=f"{' '.join(content1)}",color=discord.Colour.dark_orange()) )
+                    embeds.append( discord.Embed(title=f"登録一覧(登録数:{len(res)}) {page}ページ目",description=f"{' '.join(content1)}") )
                     x +=1
                     _index += 20
                 if mod > 0:
                     page += 1
                     content2= res[_index:_index+mod-1]
-                    embeds.append( discord.Embed(title=f"登録一覧(登録数:{len(res)}) {page}ページ目",description=f"{''.join(content2)}",color=discord.Colour.dark_orange()) )
+                    embeds.append( discord.Embed(title=f"登録一覧(登録数:{len(res)}) {page}ページ目",description=f"{''.join(content2)}") )
 
             paginator = BotEmbedPaginator(ctx, pages=embeds,control_emojis=ControlEmojis(first='⏮', previous='◀', next='▶', last='⏭', close=None))
             await paginator.run(timeout_msg='```listコマンドは100秒間だけ表示&操作可能です。再度表示&操作したい場合はもう一度コマンドを実行してください。```')
@@ -290,29 +349,94 @@ class ReferenceCommand(commands.Cog):
             await ctx.send(content='`このサーバーでは何も登録がないようです。`')
         postc()
 
+    @commands.command(name="search")
+    async def partialmatch(self,ctx,*args):
+        '''
+        部分一致を含む検索
+        '''
+        if not args:
+            await Msg.no_key(ctx)
+        else:
+            args = ' '.join(args)
+            if len(args) > 20:
+                await Msg.len_over(ctx)
+                postc()
+                return
+            
+            embeds = []
+            _id = ctx.guild.id
+            res = partial_match(serverid=_id,keyword=args)
+            if res:
+                print(res,len(res))
+                page = 1
+                embeds.append( discord.Embed(title=f"「{args}」\n 検索結果(登録数:{len(res)}) {page}ページ目",description=f"{''.join(res[0:19])}") )
+                if len(res) > 20:
+                    q = len(res) // 20
+                    mod = len(res) % 20
+                    x = 1
+                    _index = 20
+                    while x < q:
+                        page += 1
+                        content1 = res[_index:_index+20]
+                        embeds.append( discord.Embed(title=f"「{args}」\n 検索結果(登録数:{len(res)}) {page}ページ目",description=f"{' '.join(content1)}") )
+                        x +=1
+                        _index += 20
+                    if mod > 0:
+                        page += 1
+                        content2= res[_index:_index+mod-1]
+                        embeds.append( discord.Embed(title=f"「{args}」\n 検索結果(登録数:{len(res)}) {page}ページ目",description=f"{''.join(content2)}") )
+
+                paginator = BotEmbedPaginator(ctx, pages=embeds,control_emojis=ControlEmojis(first='⏮', previous='◀', next='▶', last='⏭', close=None))
+                await paginator.run(timeout_msg='`searchコマンドは100秒間だけ表示&操作可能です。再度表示&操作したい場合はもう一度コマンドを実行してください。`')
+            else:
+                await ctx.send(content= f'`キーワード:{args} で部分一致含む検索をした結果、ヒット件数0件でした。`')
+        postc()
 
     @commands.command()
-    async def author(self,ctx,*arg):
+    async def author(self,ctx,arg: Union[discord.Member,int,str] = None):
         '''
-        入力されたキーワードの登録者を表示
+        指定ユーザーがの登録一覧を表示。無指定の場合書き込み者の登録一覧
 
         '''
-        _id = ctx.guild.id
-        if arg:
-            arg = ' '.join(arg)
-            res = search_keyword(serverid = _id,keyword = arg)
-            # キーワードが20文字以上の場合は拒否
-            if len(arg) > 20:
-                await ctx.send(content='`登録されているキーワードはスペースキー含め20文字以内のはずです`')
-            else:
-                if res:
-                    await ctx.send(content=f'`キーワード:{arg}に画像を登録した人は{bot.get_user(res["userid"]).name}です`')
-                else:
-                    await ctx.send(content=f'`キーワード:{arg}で画像は登録されてません。`')
-
-        # キーワードが入力されていない場合はその旨を伝える
+        if not arg:
+            res = search_author(serverid=ctx.guild.id,userid=ctx.author.id)
+        elif type(arg) is discord.Member:
+            print(type(arg))
+            res = search_author(serverid=ctx.guild.id,userid=arg.id)
         else:
-            await ctx.send(content='`!authorを入力するときは必ずキーワードを指定してください。`')
+            await ctx.send(f'```検索するユーザーを１ユーザーのみ指定するか、貴方自身を検索する場合は何も入力しないでください。```')
+            return
+
+        if res:
+            if not arg:
+                name = ctx.author.name
+            elif type(arg) is discord.Member:
+                name = arg.name
+
+            print(res,len(res))
+            page = 1
+            embeds = []
+            embeds.append( discord.Embed(title=f"ユーザー名「{name}」\n 検索結果(登録数:{len(res)}) {page}ページ目",description=f"{''.join(res[0:19])}") )
+            if len(res) > 20:
+                q = len(res) // 20
+                mod = len(res) % 20
+                x = 1
+                _index = 20
+                while x < q:
+                    page += 1
+                    content1 = res[_index:_index+20]
+                    embeds.append( discord.Embed(title=f"ユーザー名「{name}」\n 検索結果(登録数:{len(res)}) {page}ページ目",description=f"{' '.join(content1)}") )
+                    x +=1
+                    _index += 20
+                if mod > 0:
+                    page += 1
+                    content2= res[_index:_index+mod-1]
+                    embeds.append( discord.Embed(title=f"ユーザー名「{name}」\n 検索結果(登録数:{len(res)}) {page}ページ目",description=f"{''.join(content2)}") )
+
+                paginator = BotEmbedPaginator(ctx, pages=embeds,control_emojis=ControlEmojis(first='⏮', previous='◀', next='▶', last='⏭', close=None))
+                await paginator.run(timeout_msg='`searchコマンドは100秒間だけ表示&操作可能です。再度表示&操作したい場合はもう一度コマンドを実行してください。`')
+        else:
+            await ctx.send(content= f'`ユーザー名:{arg.name or ctx.author.id}で検索した結果、ヒット件数0件でした。`')
         postc()
 
     @commands.command()
@@ -326,7 +450,7 @@ class ReferenceCommand(commands.Cog):
         # 登録がある場合は登録数を表示
         if res:
             await ctx.send(content=f'`このサーバーでの登録数は{len(res)}個です。`')
-        # キーワードに登録がない場合はそのことを伝える。
+        # キーワード登録がない場合はそのことを伝える。
         else:
             await ctx.send(content=f'`このサーバーで登録はありません。`')
         postc()
@@ -344,7 +468,7 @@ class ReferenceCommand(commands.Cog):
 if __name__ == '__main__':
     intents = discord.Intents.all()
     intents.members = True
-    bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),intents=intents,help_command= HelpCommand())
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or("-"),intents=intents,help_command= HelpCommand())
     bot.add_cog(BasicCommand(bot=bot))
     bot.add_cog(DeleteCommand(bot=bot))
     bot.add_cog(ReferenceCommand(bot=bot))
@@ -353,4 +477,4 @@ if __name__ == '__main__':
         print(f'🟠ログインしました🟠')
     check_db()
 
-bot.run( 'TOKEN')
+bot.run( 'Token')
